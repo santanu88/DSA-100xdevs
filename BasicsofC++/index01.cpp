fb5e5d53864d08c142e0e2d0a17e01bc53087904
Shantanu Sabyasachi Swain <shantanu.swain23@gmail.com>
2026-01-18 00:09:38 +0530

    1st c++program

---
+++ BasicsofC++/index01.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    cout << "hello world" <<  endl;// for another line
+    cout << "100xscholl" ;
+    //or we can do  in single line
+    cout << "hello world" << endl  << "100xschool";
+}
