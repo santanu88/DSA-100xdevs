a63f82138d4f87711654c9dbe68f619e0124d7ae
Shantanu Sabyasachi Swain <shantanu.swain23@gmail.com>
2026-01-18 00:35:07 +0530

    Arithematic Opwerators

---
+++ BasicsofC++/Arithemetic02.cpp
@@ -0,0 +1,10 @@
+//Arithematic operators
+#include <iostream>
+using namespace std;
+int main(){
+ cout << 10+5 << endl; //15
+ cout << 10-5 << endl;  //5
+ cout << 10*5 << endl;   //50
+ cout << 10/3 << endl;  //3 if we divide 2 integer the result will always be the intefger  and ion this case we get the qoutent as outpuyt
+ cout << 10%3 << endl; // if we want remainder we can usin modulo % operator to get the remainder
+}
