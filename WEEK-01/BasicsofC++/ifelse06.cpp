bc1810a7c88aa9425b5316bd9c6133183821f79f
Shantanu Sabyasachi Swain <shantanu.swain23@gmail.com>
2026-01-19 00:55:39 +0530

    if else statement

---
+++ WEEK-01/BasicsofC++/ifelse06.cpp
@@ -0,0 +1,12 @@
+//check the number is odd or not 
+#include <iostream>
+using namespace std;
+int main (){
+  int num;
+  cin >> num;
+if (num % 2 ==0)
+{
+    cout << "EVEN";
+}else
+cout << "ODD";
+}
---
+++ WEEK-01/BasicsofC++/operators05.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+bool  ans1 = 5==5 ;// true ---denoted as 1
+bool  ans2 = 5!=6;// true
+bool  ans3 = 3<=5;// false---denoted as 0
+
+cout << ans1 << endl;
+cout << ans2 << endl;
+cout << ans3 << endl;
+
+
+// if we assign any non-zero value it always be true ->1
+
+bool  dev = 0; // false-0   onnly zero means in false
+bool  dev =4469463563; // true-1
+bool  dev =-14252; // true-1
+}
