b0ef328fee8798808b3c414efac065848aee56a9
Shantanu Sabyasachi Swain <shantanu.swain23@gmail.com>
2026-01-18 10:13:55 +0530

    decimal interger

--- BasicsofC++/Arithemetic02.cpp
+++ BasicsofC++/Arithemetic02.cpp
@@ -6,5 +6,12 @@ int main(){
  cout << 10-5 << endl;  //5
  cout << 10*5 << endl;   //50
  cout << 10/3 << endl;  //3 if we divide 2 integer the result will always be the intefger  and ion this case we get the qoutent as outpuyt
- cout << 10%3 << endl; // if we want remainder we can usin modulo % operator to get the remainder
+ cout << 10%3 << endl; //  
+
+
+ ///if we want an decimal answer then what to do
+ //--we can  use float or one number should be floating point number  lik3 3.0 or 10.0
+
+ cout << 10.0/3 << endl;// 3.33////----or
+ cout << 10/3.0 << endl;///3.3333
 }
