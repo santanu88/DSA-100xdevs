03691c02d11336057a81c9d7803ff213167d94c9
Shantanu Sabyasachi Swain <shantanu.swain23@gmail.com>
2026-01-18 18:35:45 +0530

    Sum of two numbers

---
+++ BasicsofC++/Variables03.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+int main(){
+int age =20;
+cout << age << endl;//20
+//if we give any decimal value to integer the it goive the output
+int age =20.89;
+cout << age << endl;//20 it will ignore the decimal value
+
+//to store decimal part we have tu use float or double
+float  age =20.89; //or
+double age =20.89;
+}
---
+++ BasicsofC++/sum04.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int a=10;
+int  b=20;
+//if we want user to give number 
+//then
+int a;
+int b;
+cin >> a;
+cin >> b;
+
+
+cout<< a + b << endl;
+}
+
+//cin helps to take input from the user
