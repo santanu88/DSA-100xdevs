079a07faf253c8235e92dddab316363c55b57cb1
Shantanu Sabyasachi Swain <shantanu.swain23@gmail.com>
2026-01-20 23:48:33 +0530

    Pass or fail -if else condition

---
+++ WEEK-01/Assignment-01/m.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int marks;
+    cin >> marks;
+
+    if (marks >= 35)
+    {
+        cout << "PASS" <<endl;
+    }else
+    cout << "Fail" <<endl;
+
+}
