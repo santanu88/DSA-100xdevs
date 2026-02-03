2d6e06cb94d7b8f1a7109aa2adab2d326091199a
Santanu Swain <shantanu.swain23@gmail.com>
2026-02-04 00:16:35 +0530

    count numbers

--- WEEK-02/Assignment-02/G.cpp
+++ WEEK-02/Assignment-02/G.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 using namespace std;
-int main(){
-    
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    long long X;
+    int pos = 0, neg = 0, eve = 0, odd = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> X;
+
+        if (X > 0)
+            pos++;
+        else if (X < 0)
+            neg++;
+
+        if (X % 2 == 0)
+            eve++;
+        else
+            odd++;
+    }
+    cout << pos << endl << neg << endl << eve << endl << odd << endl;
+
+    return 0;
 }
